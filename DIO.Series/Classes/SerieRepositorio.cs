13249f0a987261a76e71337e3bd8f46122a4cc15
jeankrausejean <jeankrausejean@gmail.com>
2021-11-25 23:09:17 -0300

    Implementação Repositorio de series.

--- DIO.Series/Classes/SerieRepositorio.cs
+++ DIO.Series/Classes/SerieRepositorio.cs
@@ -2,11 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DIO.Series.Interfaces;
 
-namespace DIO.Series.Classes
+namespace DIO.Series
 {
-    public class SerieRepositorio
+    public class SerieRepositorio : IRepositorio<Serie>
     {
-        
+        private List<Serie> listaserie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Proximo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
